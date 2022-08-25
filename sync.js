f297ce80304f7553e88fb3757577a510bbf734e7
rjh <jh.ra@kisos.net>
2022-08-25 16:44:06 +0900

    f1,f2,f3 함수를 순차적으로 실행하는 동기 프로그래밍 예제 추가

---
+++ sync.js
@@ -0,0 +1,8 @@
+const f1 = () => console.log(`f1 called`);
+const f2 = () => console.log(`f2 called`);
+const f3 = () => console.log(`f3 called`);
+
+// call f1, f2, f3 sequentially
+f1();
+f2();
+f3();
