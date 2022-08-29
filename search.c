02464e61e2a862257e35868d42a8bf63350eec1b
rjh <jh.ra@kisos.net>
2022-08-29 16:08:14 +0900

    5보다 큰 숫자를 탐색하는 예제 추가

---
+++ search.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void search(int array[], size_t length) {
+	int i;
+
+	for (i = 0; i < length; i++) {
+		if (array[i] > 5) {
+			break;
+		}
+	}
+
+	if (i < length) {
+		printf("Item %d\n", i);
+	}
+	else {
+		printf("Not found\n");
+	}
+}
+
+void main() {
+	int arr[] = {5, 8, 3};
+	search(arr, 3);
+}
